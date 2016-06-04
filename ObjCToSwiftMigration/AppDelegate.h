9684621c0d1c8527be4e6e6483ad5341bb3f0100
Lawrence F MacFadyen <larry.macfadyen@gmail.com>
2016-06-04 10:31:47 -0400

    Initial Commit

---
+++ ObjCToSwiftMigration/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ObjCToSwiftMigration
+//
+//  Created by Lawrence F MacFadyen on 2016-06-04.
+//  Copyright © 2016 LawrenceM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ObjCToSwiftMigration/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ObjCToSwiftMigration
+//
+//  Created by Lawrence F MacFadyen on 2016-06-04.
+//  Copyright © 2016 LawrenceM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
